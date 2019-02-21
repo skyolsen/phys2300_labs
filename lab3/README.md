89f77ceb32ff6d229ca185a31ee0665be6ffbdf2
Skylar <skyolsen@hotmail.com>
2019-02-20 22:35:44 -0700

    complete assignment

--- lab3/README.md
+++ lab3/README.md
@@ -1,2 +1,7 @@
 # phys2300_labs
 Scientific Computing Labs
+
+Skylar Olsen
+02/20/2019
+Homework 3, Plotting Temperatures
+
--- lab3/lab3_template.py
+++ lab3/lab3_template.py
@@ -23,11 +23,11 @@ import matplotlib.pylab as plt
 import numpy as np
 
 # Pseudocode:
-# 1) get the name of the data file from the user on the command line
-# 2) open the data file
-# 3) read the first line of data and throw it away (it is the header info the computer doesn't need)
-#       from all the remaining lines:
-#       read in the date (index 2) and temperature (index 3)
+#J 1) get the name of the data file from the user on the command line 
+#J 2) open the data file
+#J 3) read the first line of data and throw it away (it is the header info the computer doesn't need)
+#J       from all the remaining lines:
+#J       read in the date (index 2) and temperature (index 3)
 #       parse the date string into year, month, day
 #       convert year, month, day into decimal years for plotting (this would make a great function!)
 #       (need to pay attention to leap years here!)
@@ -46,6 +46,16 @@ def parse_data(infile):
     wdates = []             # list of dates data
     wtemperatures = []      # list of temperarture data
 
+    #TODO:
+    try:
+        with open(infile, mode = 'r') as file:
+            file.readline() # to extract the first line so we can skip it
+            for line in file:
+                line_words = line.split() # split record with space seperator
+                wdates.append(line_words[2])
+                wtemperatures.append(float(line_words[3]))
+    except:
+        print('Error Parsing Data')
     return wdates, wtemperatures
 
 
@@ -60,8 +70,52 @@ def calc_mean_std_dev(wdates, wtemp):
     means = []
     std_dev = []
 
+    #TODO:
+    #list of lists to hold monthly temperatures
+    monthDayTemp = [ [],[],[],[],[],[],[],[],[],[],[],[] ]
+
+    # Get the month to determin index of list. Then append the temperature the the list of that index.
+    try:
+        i = 0
+        sizeOfList = len(wdates) 
+        while i < sizeOfList:
+            j = 1
+            while j < 13:
+                if int(wdates[i][4:6]) == j: 
+                    monthDayTemp[j-1].append(wtemp[i])
+                    break
+                j += 1
+            i += 1
+
+    #Calculate the temperature and std dev average
+        for monthTemps in monthDayTemp:
+            means.append(np.average(monthTemps))
+            std_dev.append(np.std(monthTemps))
+
+    except:
+        print('Error with data when calculating Mean and StdDev')
+        exit(0)
+
     return means, std_dev
 
+ #TODO:
+def calc_year_daytemps(wdates):
+    """
+    Cast years to int variable. Append to list
+    :param years: List with year fields
+    :return: Decimal Years 
+    """
+    years = []
+
+    try:
+        sizeOfList = len(wdates)
+        i = 0
+        while i < sizeOfList:
+            years.append(int(wdates[i][0:4]))
+            i += 1
+    except:
+        print('Error calculating decimal year')
+    return years
 
 
 def plot_data_task1(wyear, wtemp, month_mean, month_std):
@@ -79,6 +133,9 @@ def plot_data_task1(wyear, wtemp, month_mean, month_std):
     plt.plot(wyear, wtemp, "bo")
     plt.ylabel("Temperature, F")
     plt.xlabel("Decimal Year")
+    plt.ylim([-20, 100])
+    plt.xlim([1970, 2015])
+
 
     plt.subplot(2, 1, 2)                # select second subplot
     plt.ylabel("Temperature, F")
@@ -105,13 +162,19 @@ def plot_data_task2(xxx):
 
 
 def main(infile):
-    weather_data = infile    # take data file as input parameter to file
+    weather_data = infile
+
+    # take data file as input parameter to file
     wdates, wtemperatures = parse_data(weather_data)
+
     # Calculate mean and standard dev per month
     month_mean, month_std = calc_mean_std_dev(wdates, wtemperatures)
+
     # TODO: Make sure you have a list of:
     #       1) years, 2) temperature, 3) month_mean, 4) month_std
-    plot_data_task1(wyear, wtemp, month_mean, month_std)
+    wyear = calc_year_daytemps(wdates)
+    plot_data_task1(wyear, wtemperatures, month_mean, month_std)
+
     # TODO: Create the data you need for this
     # plot_data_task2(xxx)
 
@@ -120,6 +183,13 @@ def main(infile):
 if __name__ == "__main__":
     # infile = 'data/CDO6674605799016.txt'  # for testing
     # Note: the 0th argument is the program itself.
-    infile = sys.argv[1]
+    
+    try:
+        infile = sys.argv[1]
+    except:
+        print('No File Passed')
+        exit(0)
+    
+    #infile = sys.argv[1]
     main(infile)
     exit(0)
